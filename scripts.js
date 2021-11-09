17972a91133f936512c229652f7e005fe9917e61
upoudel888 <upoudel888@gmail.com>
2021-11-09 17:47:49 +0545

    Now I'll have to add a font

--- scripts.js
+++ scripts.js
@@ -2,21 +2,25 @@
 let turn = 0; // 1 is X and 0 is O
 let inputCount = 0; //for checking draw
 let winStatus = 0;  // 1 means there's a winner
+let highlightPos = new Array();//to show which order of arrangement of X or O won the game
+
 let board = [
     [1,2,3],
     [4,5,6],
     [7,8,9]
 ]
 
+let emptyPos = document.querySelectorAll('.box');
+let resultBox = document.querySelector('.result');
+let restart = document.querySelector('.reload');
+
 
 //UPDATES X and 0 in the board and returns status if updated
 function update(pos){
     let a = turn ? 'X':'O';
-    console.log(a);
     if(!winStatus){
         board[pos[0]][pos[1]] = a;
         inputCount +=1;
-        console.log(board);
         return true;
     }
     return false;
@@ -26,58 +30,124 @@ function update(pos){
 function checkWin(){
     //main diagonal check
     if(board[0][0] == board[1][1] && board[1][1]==board[2][2]){
+        highlightPos = [
+            [0,0],
+            [1,1],
+            [2,2]
+        ]
         return true;
     } 
     // other diagonal check
     if(board[0][2] == board[1][1] && board[1][1] ==board[2][0]){
+        highlightPos = [
+            [0,2],
+            [1,1],
+            [2,0]
+        ]
         return true;
        
     } 
     //row and column check
     for(let i = 0; i<3;i++){
         if(board[i][0] == board[i][1] && board[i][1] ==board[i][2]){
+            highlightPos = [
+                [i,0],
+                [i,1],
+                [i,2]
+            ]
             return true;
            
         } 
         
         if(board[0][i] == board[1][i] && board[1][i]==board[2][i]){
+            highlightPos = [
+                [0,i],
+                [1,i],
+                [2,i]
+            ]
             return true;
         } 
     }
     return false;
 }
 
+function highlightWin(){
+    let count = 0;
+    for(let i=0; i<3; i++){
+        for(let j =0; j<3; j++){
+            for(let k=0; k<3;k++){
+                if(JSON.stringify([i,j])==JSON.stringify(highlightPos[k])){
+                    emptyPos[count].classList.add('win');
+                }
+            }
+            count++;
+        }
+    }
+}
 
-let emptyPos = document.querySelectorAll('td');
+
+
 emptyPos.forEach(emptyPos =>{
     emptyPos.addEventListener('click',function(){
-        var pos = emptyPos.textContent;
+        var pos = emptyPos.firstChild.textContent;
+
         //checking if X or O has already been assigned to the position
-        if(pos.length != 1){
-            
+        if(pos.length != 1 && winStatus != 1){
             if(update(pos)){
                 let a = turn ? 'X':'O';
-                emptyPos.textContent = a;
-                emptyPos.style.display = "inside";
+                emptyPos.firstChild.textContent = a;
+                emptyPos.firstChild.style.visibility = "visible";
                 turn = turn ? 0:1;
             }
+
+            //checking win and draw condition
             if(inputCount >= 5){
                 if(checkWin()){
                     winStatus = 1;
                     turn = turn ? 0:1;
                     let a = turn ? 'X':'O';
 
-                    alert(a + " has won the game");
-                    location.reload();
+                    highlightWin();
+
+                    resultBox.innerHTML = a + " WON !! ";
+                    resultBox.style.visibility= "visible";
+                    resultBox.classList.add('win');
+                    
                 }else if(inputCount == 9){
-                    alert("Draw");
-                location.reload();
+                    resultBox.innerHTML = "DRAW";
+                    resultBox.style.visibility= "visible";
+                    resultBox.classList.add('draw');
                 }
             }
             
         }
     })
-})
+    
+});
+
+//reset everything to beginning
+restart.addEventListener("click",function(){
+    
+    turn = 0; 
+    inputCount = 0; 
+    winStatus = 0; 
+    board = [
+        [1,2,3],
+        [4,5,6],
+        [7,8,9]
+    ];
+    let count = 0;
+    for(let i=0; i<3; i++){
+        for(let j =0; j<3; j++){
+            emptyPos[count].firstChild.innerHTML = i.toString() + j.toString();
+            emptyPos[count].firstChild.style.visibility = "hidden";
+            emptyPos[count].classList.remove('win');
+            count++;
+        }
+    }
+    resultBox.style.visibility= "hidden";
+    
+});
 
 
 
